600bb3e391685396425520d66d7f34487652f805
Shane Chagpar <42649692+MajorIncident@users.noreply.github.com>
2021-04-14 10:54:41 -0400

    Updated to use OS working directory - new progress messages

--- #KT Books Watermarker.py
+++ #KT Books Watermarker.py
@@ -2,10 +2,13 @@
 #Author: Shane Chagpar
 #Inception Date: 2020 12 08
 #Formats PDFs with a watermark and emails them to a mail merge list from excel
-#Requires Libraries: Pypdf2, fpdp, pypiwin32, pikepdf
+#Requires Libraries: Pypdf2, fpdf, pypiwin32, pikepdf --> Requirements.txt generated from pipreqs library
+#Installation: pip install -r requirements.txt
 #Inputs: list.csv, cards.pdf, cases.pdf, notes.pdf, extra.pdf in same folder
 #Outputs: zip files sent to email address in outoook 
 #Limitations: Outlook must be open when run
+
+#Future: Use setuptools to build package distribtor 
 
 #CUSTOMIZATIONS ----------------------------------------------------------
 copyrightLine = "Copyright (C) Kepner-Tregoe All Rights Reserved"
@@ -18,7 +21,6 @@ auditCCEmail = ""
 pdfPassword = "KTPassword"
 
 #GLOBAL VARIABLES -------------------------------------------------------------
-filePathDir = r"C:\Users\SCHAGPAR\OneDrive\Documents\Personal\Filing Cabinet\Coding Projects\KT Books Watermarker"
 auditReport = ""
 
 #LIBRARY IMPORTS ---------------------------------------------------------------
@@ -28,6 +30,7 @@ from csv import reader
 #Libraries for File Handling
 import os.path
 import os, errno
+from pathlib import Path
 
 #Libraries for Creating a Watermark
 import fpdf #pip install fdpf
@@ -49,7 +52,8 @@ from zipfile import ZipFile
 #SUBROUTINES -------------------------------------------------------------
 def read_list(listFileName = ''):
     # open file in read mode
-    with open(listFileName, 'r') as read_obj:
+    p = Path(__file__).with_name(listFileName)
+    with open(p, 'r') as read_obj:
         csv_reader = reader(read_obj)
         header = next(csv_reader)
         # Check file as empty
@@ -62,20 +66,29 @@ def read_list(listFileName = ''):
             # Iterate over each row after the header in the csv
             for row in csv_reader:
                 # row variable is a list that represents a row in csv
-                print(row)
-
+                print("START PROCESS - Found ", row)
+            
                 #DO THE WORK
                 #audit_log("Materials for " + row[0] + ' ' + row[1] + " started")
+                print("1/9 Watermarking for", row[0] + ' ' + row[1])
                 create_watermark('These materials produced for ' + row[0] + ' ' + row[1] + ' on ' + today.strftime("%B %d, %Y") + ' and may not be redistributed.')
-                merge_pdf(row[0] + row [1], 'notes.pdf')
+                print("2/9 Starting on Notes for", row[0] + ' ' + row[1])
+                merge_pdf(row[0] + row [1], 'notes.pdf') 
+                print("3/9 Starting on Cases for", row[0] + ' ' + row[1])
                 merge_pdf(row[0] + row [1], 'cases.pdf')
+                print("4/9 Starting on Cards for", row[0] + ' ' + row[1])
                 merge_pdf(row[0] + row [1], 'cards.pdf')
+                print("5/9 Starting on Extras for", row[0] + ' ' + row[1])
                 merge_pdf(row[0] + row [1], 'extra.pdf')
                 #audit_log("Materials for " + row[0] + ' ' + row[1] + " Watermarked")
+                print("6/9 Creating compressed package for", row[0] + ' ' + row[1])
                 zip_pdf(row[0] + row [1])
                 #audit_log("Materials for " + row[0] + ' ' + row[1] + " Compressed")
+                print("7/9 Emailing", row[0] + ' ' + row[1])
                 create_mail("Hi " + row[0] + " " + row [1] + ", " + emailBody, emailSubject, row[2], row[0] + row [1] + '.zip', send=False)
+                print("8/9 Documenting Audit Log for", row[0] + ' ' + row[1])
                 audit_log("Materials for " + row[0] + ' ' + row[1] + " Sent")
+                print("9/9 Cleaning up temporary files for", row[0] + ' ' + row[1])
                 cleanup_folder(row[0] + row [1])
                 #audit_log("Materials for " + row[0] + ' ' + row[1] + " Removed from Local Drive")
                 
@@ -94,12 +107,14 @@ def merge_pdf(participantName, pdf_file = ''):
 
     #Open and read original file
     try:
-        input_file = open(pdf_file,'rb')
+        p = Path(__file__).with_name(pdf_file)
+        input_file = open(p,'rb')
         input_pdf = PyPDF2.PdfFileReader(input_file)
         output = PyPDF2.PdfFileWriter()
         
         #Open and read watermark file
-        watermark_file = open(watermark,'rb')
+        p = Path(__file__).with_name(watermark)
+        watermark_file = open(p,'rb')
         watermark_pdf = PyPDF2.PdfFileReader(watermark_file)
 
         #Watermark each page in the PDF
@@ -114,7 +129,8 @@ def merge_pdf(participantName, pdf_file = ''):
             output.addPage(pdf_page)
 
         #Save output from memory to disk
-        merged_file = open(merged_file,'wb')
+        p = Path(__file__).with_name(merged_file)
+        merged_file = open(p,'wb')
         output.write(merged_file)
 
         #Cleanup
@@ -132,7 +148,8 @@ def merge_pdf(participantName, pdf_file = ''):
     return 0
 
 def encrypt_PDF(sourceFileName = ''):
-    pdf = pikepdf.Pdf.open(sourceFileName, allow_overwriting_input=True)
+    p = Path(__file__).with_name(sourceFileName)
+    pdf = pikepdf.Pdf.open(p, allow_overwriting_input=True)
     pdfRestrictions = pikepdf.Permissions(accessibility=False, extract=False, print_lowres=False, print_highres=False, modify_annotation=False, modify_assembly=False, modify_form=False, modify_other=False)
     pdf.save(sourceFileName, encryption=pikepdf.Encryption(user="", owner=pdfPassword, allow=pdfRestrictions))
 
@@ -144,7 +161,11 @@ def create_mail(text, subject, recipient, attachmentName, send=True):
     mail.CC = ccEmail
     mail.Subject = subject
     mail.HtmlBody = text
+    
+    filePathDir = os.getcwd()
     attachment1 = os.path.join(filePathDir, attachmentName)
+    #attachment1 = Path(__file__).with_name(attachmentName) <--- how everything else is setup but returns windowspath so not used
+    
     mail.Attachments.Add(Source=attachment1)
     
     if send:
@@ -179,6 +200,7 @@ def cleanup_folder(participantName = ''):
 
 def cleanup_file(filename):
     try:
+        p = Path(__file__).with_name(filename)
         os.remove(filename)
     except OSError as e: # this would be "except OSError, e:" before Python 2.6
         if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
